be9057c20104c22c57df702b8e98f8ce063277d1
Dave Hill <dave.hill@outlook.com>
2017-08-19 21:50:57 +0100

    Basic working example
    SCSS to CSS added

--- dist/js/app.js
+++ dist/js/app.js
@@ -65,9 +65,18 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(1);
+
+const test = "moo";
+
+
+/***/ }),
+/* 1 */
 /***/ (function(module, exports) {
 
-
+// removed by extract-text-webpack-plugin
 
 /***/ })
 /******/ ]);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,29 @@
-var path = require('path');
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: './src/js/app.js',
+  entry:  {
+    app: './src/js/app.js',
+    //scss: './src/scss/main.scss'
+  },
   output: {
     path: path.resolve(__dirname, 'dist/js'),
-    filename: 'app.js'
-  }
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          //resolve-url-loader may be chained before sass-loader if necessary
+          use: ['css-loader', 'sass-loader']
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('../css/main.css')
+  ]
 };
